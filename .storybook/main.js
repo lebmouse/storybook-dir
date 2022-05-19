56343b4a589ae2f0e1dd4dc422d166b384320ba2
lebmouse <youngkwon@croquis.com>
2022-05-19 10:03:32 +0900

    chore:  title 나누기

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,29 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    // "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    {
+      directory: "../src/component/ui",
+      // 👇 The titlePrefix field will generate automatic titles for your stories
+      titlePrefix: "ui",
+      // 👇 Storybook will load all files that contain the stories extension
+      files: '*.stories.*',
+    },
+    {
+      directory: "../src/component/pure",
+      // 👇 The titlePrefix field will generate automatic titles for your stories
+      titlePrefix: "pure",
+      // 👇 Storybook will load all files that contain the stories extension
+      files: '*.stories.*',
+    },
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
